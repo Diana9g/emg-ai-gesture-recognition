65f2e447a2a406a6dc9dc174624257bed7199cd9
Diana9g <132343738+Diana9g@users.noreply.github.com>
2026-03-29 13:37:28 +0200

    GestureListener.cs

--- unity/scripts/GestureListener.cs
+++ unity/scripts/GestureListener.cs
@@ -1,1 +1,25 @@
 
+using UnityEngine; 
+using Unity.Robotics.ROSTCPConnector; 
+using Unity.Robotics.ROSTCPConnector.MessageGeneration; 
+using RosMessageTypes.Std; 
+public class GestureListener : MonoBehaviour 
+{ 
+ROSConnection ros; 
+public TrajectoryPlanner planner;  // връзка към скрипта, който изпраща траектория 
+void Start() 
+{ 
+ros = ROSConnection.GetOrCreateInstance(); 
+ros.Subscribe<Int32Msg>("/recognized_gesture", OnGestureReceived);
+    } 
+ 
+    void OnGestureReceived(Int32Msg msg) 
+    { 
+        Debug.Log("Gesture received: " + msg.data); 
+        if (msg.data == 3) 
+        { 
+            planner.PublishJoints(); 
+ 
+        } 
+    } 
+}
